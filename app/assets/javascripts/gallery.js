24fd06ef629c90902d6fa22ee866604bca7e4bc1
Juan M Salamanca <jumisz.dev@gmail.com>
2014-11-18 22:04:03 +0000

    Fixed menu styles for gallery

--- app/assets/javascripts/gallery.js
+++ app/assets/javascripts/gallery.js
@@ -1,11 +1,7 @@
 var ready = function(){
  
-  var href = window.location.href;
-  var currentPage =href.substr(href.lastIndexOf('/') + 1);
-  currentPage = currentPage.replace(/\b[a-z]/g, function(letter) {
-    return letter.toUpperCase();
-  })
-  $('a[href$='+currentPage+']').addClass('bold');
+ 
+  $('a[href$="gallery"]').addClass('bold');
 
   Galleria.run('.galleria', {
     flickr: 'set:72157625790610133',
