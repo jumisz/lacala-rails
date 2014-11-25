3ac25b392cc98941d12ccc643d1304ed6fae29fa
Juan M Salamanca <jumisz.dev@gmail.com>
2014-11-25 20:16:11 +0000

    precompiling assets again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,9 +25,7 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
-  config.assets.enabled = false
-  config.assets.debug = true
-  config.assets.precompile = []
+  
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
